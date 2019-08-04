40b7d08901d2406feb4b6a709184ea6090e17958
Nick <nickkarmatskikh@gmail.com>
2019-08-04 18:45:07 +0500

    add produces header

--- src/main/java/com/evy/controller/ObjectController.java
+++ src/main/java/com/evy/controller/ObjectController.java
@@ -3,6 +3,7 @@ package com.evy.controller;
 import com.evy.model.MapObject;
 import com.evy.service.ObjectService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class ObjectController {
 
     private final ObjectService objectService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<MapObject> getObject(@PathVariable("id") Integer id) {
         return objectService.getObject(id);
     }
--- src/main/java/com/evy/controller/PersonController.java
+++ src/main/java/com/evy/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.evy.controller;
 import com.evy.model.Person;
 import com.evy.service.PersonService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class PersonController {
 
     private final PersonService personService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Person> getPerson(@PathVariable("id") Integer id) {
         return personService.getPerson(id);
     }
